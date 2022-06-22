b52fc3545c8a990864691d67cccfc5f128e94fc8
Nicolletr <78945498+Nicolletr@users.noreply.github.com>
2022-06-22 15:22:49 -0500

    AW-2P

---
+++ 2Parcial/Clase/12Microservicios/microservicio/producto/index.js
@@ -0,0 +1,4 @@
+const servidor  = require('./src/app')
+servidor.listen(process.env.PORT, ()=>{
+    console.log(`Microservicio de productos corriendo en puerto ${process.env.PORT}`);
+})
---
+++ 2Parcial/Clase/12Microservicios/microservicio/usuarios/index.js
@@ -0,0 +1,4 @@
+const servidor  = require('./src/app')
+servidor.listen(process.env.PORT, ()=>{
+    console.log(`Microservicio de usuarios corriendo en puerto ${process.env.PORT}`);
+})
