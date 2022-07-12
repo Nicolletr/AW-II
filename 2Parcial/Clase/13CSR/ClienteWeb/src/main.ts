ff369c780c23493532be6def6ed3ab48b9f5377f
Nicolletr <78945498+Nicolletr@users.noreply.github.com>
2022-07-12 17:20:38 -0500

    AW-2P

--- 2Parcial/Clase/13CSR/ClienteWeb/src/main.ts
+++ 2Parcial/Clase/13CSR/ClienteWeb/src/main.ts
@@ -1,20 +1,19 @@
 import './style.css'
 import axios from 'axios'
-import { IResProducto, Producto  } from './interfaces/IProducto'
+import { IResProducto, Producto } from './interfaces/IProducto'
 
-const httpAxios= axios.create({
-  baseURL:`http://localhost:2500/v1/sextoa/api/`
+const httpAxios = axios.create({
+  baseURL: `http://localhost:2500/v1/sextoa/api/`
 })
-
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 //#region 
 const etiqueta = document.createElement("label");
-etiqueta.textContent=`ID`
+etiqueta.textContent = `ID`
 const input = document.createElement("input");
-input.id="id"
-etiqueta.htmlFor="id"
+input.id = "id"
+etiqueta.htmlFor = "id"
 //#endregion
 
 app.appendChild(etiqueta);
@@ -48,49 +47,81 @@ const consultar = document.querySelector<HTMLButtonElement>("#consultar")!
 const cuerpo = document.querySelector<HTMLDivElement>('#cuerpo')!
 //#endregion
 
-nuevo.addEventListener('click',()=>{
-  id.value=""
-  nombre.value=""
-  precio.value=""
-  costo.value=""
-  stock.value=""
-  minimo.value=""
-  estado.value=""
+nuevo.addEventListener('click', () => {
+  id.value = ""
+  nombre.value = ""
+  precio.value = ""
+  costo.value = ""
+  stock.value = ""
+  minimo.value = ""
+  estado.value = ""
 
 })
 
-consultar.addEventListener('click', async ()=>{
-     const respproductos:IResProducto =  await (await httpAxios.get<IResProducto>('productos')).data
-     console.log(respproductos);
-     const { productos } = respproductos
+consultar.addEventListener('click', async () => {
+  const respproductos: IResProducto = await (await httpAxios.get<IResProducto>('productos')).data
+  console.log(respproductos);
+  const { productos } = respproductos
 
-     const tabla =  document.createElement('table');
-     tabla.id="tabla"
-     tabla.border="1"
+  const tabla = document.createElement('table');
+  tabla.id = "tabla"
+  tabla.border = "1"
 
-     for ( const producto of productos )
-     {
-        const row = tabla.insertRow()
-        const celda = row.insertCell()
-        celda.innerHTML
-        =` <button class="boton" value='${ producto._id }'>
+  for (const producto of productos) {
+    const row = tabla.insertRow()
+    const celda = row.insertCell()
+    celda.innerHTML
+      = ` <button class="boton" value='${producto._id}'>
          ${producto.nombre} </button>`;
 
-        const celda2= row.insertCell()
-        celda2.innerHTML = `${producto.precio}`
-     }
-     cuerpo.innerHTML=""
-     cuerpo.appendChild(tabla)
-     
-     document.querySelectorAll('.boton').forEach( (ele : Element )  =>{
+    const celda2 = row.insertCell()
+    celda2.innerHTML = `${producto.precio}`
+  }
+  cuerpo.innerHTML = ""
+  cuerpo.appendChild(tabla)
 
-        (ele as HTMLButtonElement).addEventListener('click',()=>
-        {
-          // httpAxios.get(`productos/62ba6c56f371a5cffcfc5ddb`)
-          // 
-          
-          
-        })
-      
-     })
+  document.querySelectorAll('.boton').forEach((ele: Element) => {
+
+    ele.addEventListener('click', async () => {
+      const { data } = await httpAxios.get<Producto>(`productos/${(ele as HTMLButtonElement).value
+        }`)
+      console.log(data);
+      nombre.value = data.nombre;
+      precio.value = data.precio.toString();
+      costo.value = data.costo.toString();
+      minimo.value = data.minimo.toString();
+      stock.value = data.stock.toString();
+      estado.value = data.estado!.toString();
+      id.value = data._id!
+    })
+
+  })
+})
+const asignarValores = () => {
+  const data: Producto = {
+    nombre: nombre.value,
+    costo: Number(costo.value),
+    precio: Number(precio.value),
+    minimo: Number(minimo.value),
+    stock: Number(stock.value),
+  }
+  return data;
+}
+//trim para borrar los espacios en blanco
+grabar.addEventListener('click', async () => {
+  const data = asignarValores()
+  if (id.value.trim().length > 0) {
+    const respproductos: Producto = await (await httpAxios.put(`productos/${id.value}`, data)).data
+    console.log(`El producto ${respproductos.nombre} fue modificado con éxito`);
+    return;
+  }
+  try {
+    const respproductos: Producto = await (await httpAxios.post<Producto>(`productos`, data)).data
+    console.log(`El producto ${respproductos.nombre} fue insertado con éxito`);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log(`Error en axios`);
+    }
+    console.log(error);
+  }
 })
